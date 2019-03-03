f3df7fd307fe775701be22397e41ebc7b8cdc7e8
Jamie Williams <mail@jamiewilliams.co.uk>
2019-03-03 17:24:29 +0000

    Update Map

--- src/Reusable/Map/index.jsx
+++ src/Reusable/Map/index.jsx
@@ -4,18 +4,16 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import ReactMapGL from 'react-map-gl';
 import DeckGL, { GeoJsonLayer } from 'deck.gl';
-import { Row, Col } from 'reactstrap';
+import { Row, Col, Input } from 'reactstrap';
 
 // Components
-import IndividualMarker from './components/IndividualMarker';
+// import IndividualMarker from './components/IndividualMarker';
 
 // Actions
-import { changeMapViewport, getMapData } from './redux/actions';
-import { GET_MAP_DATA } from './redux/actionCreators';
+import { changeMapViewport } from './redux/actions';
 
 // Data
-import individualMapLoop from './data/individuals-map-loop.json';
-import IndividualCard from '../Individual/components/Card';
+import mapData from './data/mapDatav4.json';
 
 
 class Map extends Component {
@@ -27,25 +25,9 @@ class Map extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      index: 0,
+      type: 'GID_1',
+      party: 'totalValidVotes',
     };
-  }
-
-  componentDidMount() {
-    const {
-      dispatch,
-    } = this.props;
-
-    window.addEventListener('resize', this.handleResizeViewport);
-
-    dispatch(getMapData())
-      .then((action) => {
-        if (action.type === GET_MAP_DATA.SUCCESS) {
-          this.setState({
-            interval: setInterval(this.handleRotateElements, 3000),
-          });
-        }
-      });
   }
 
   componentWillUnmount() {
@@ -53,25 +35,10 @@ class Map extends Component {
     clearInterval(this.state.interval);
   }
 
-  handleResizeViewport = () => {
-    const { dispatch, map } = this.props;
-    dispatch(changeMapViewport({
-      ...map.viewport,
-      width: '100%',
-    }));
-  };
-
-  handleRotateElements = () => {
-    const { index } = this.state;
-    if (index < individualMapLoop.length - 1) {
-      this.setState(prevState => ({
-        index: prevState.index + 1,
-      }));
-    } else {
-      this.setState({
-        index: 0,
-      });
-    }
+  handleOnChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
   };
 
   handleOnViewportChange = (newMapViewport) => {
@@ -82,49 +49,98 @@ class Map extends Component {
     dispatch(changeMapViewport(newMapViewport));
   };
 
+  handleResizeViewport = () => {
+    const { dispatch, map } = this.props;
+    dispatch(changeMapViewport({
+      ...map.viewport,
+      width: '100%',
+    }));
+  };
+
+  handleRotateElements = () => {};
+
   handleGetFillColor = (f) => {
-    const { index } = this.state;
-    const locationId = individualMapLoop[index].location_id;
-    if (locationId === f.properties.CIRCUITO) {
-      return [232, 241, 242];
+    const {
+      locationId, hover, type,
+    } = this.state;
+    if (locationId === f.properties[type] && hover) {
+      return [255, 255, 255];
     }
-    return [23, 55, 83];
+    const colorNum = Math.floor((f.properties.electoralData['2014'].Presidente.totalValidVotes / 1854202) * 255);
+    return [colorNum, colorNum, colorNum];
   };
 
   handleGetElevation = (f) => {
-    const { index } = this.state;
-    const locationId = individualMapLoop[index].location_id;
-    if (locationId === f.properties.CIRCUITO) {
-      return 20000;
-    }
-    return 0;
+    const { party } = this.state;
+    return f.properties.electoralData['2014'].Presidente[party] / 30;
   };
 
-  renderLayers = () => {
-    const {
-      map,
-    } = this.props;
+  handleOnHover = (e) => {
+    if (e.object) {
+      this.setState(prevState => ({
+        locationId: e.object.properties[prevState.type],
+        hover: true,
+        object: e.object,
+        x: e.x,
+        y: e.y,
+        lngLat: e.lngLat,
+      }));
+    } else {
+      this.setState({
+        hover: false,
+      });
+    }
+  };
 
-    return new GeoJsonLayer({
-      data: map.data,
+  handleGetCursor = () => (this.state.hover ? 'pointer' : 'move');
+
+  renderTooltip = () => {
+    const {
+      object, x, y, hover, locationId, type,
+    } = this.state;
+
+    return (object && hover) && (
+      <div
+        className="map__tooltip__wrapper shadow"
+        style={{
+          position: 'absolute', zIndex: 1, pointerEvents: 'none', left: x, top: y - 80,
+        }}
+      >
+        <p>{mapData.features.filter(f => f.properties[type] === locationId)[0].properties.NAME_1}</p>
+      </div>
+    );
+  };
+
+  renderLayers = () =>
+    new GeoJsonLayer({
+      data: mapData,
       opacity: 2,
       filled: true,
       wireframe: true,
       extruded: true,
-      getLineColor: () => [255, 255, 255],
+      pickable: true,
+      onHover: e => this.handleOnHover(e),
+      getLineColor: [100, 100, 100],
       getFillColor: f => this.handleGetFillColor(f),
       getElevation: f => this.handleGetElevation(f),
       updateTriggers: {
         getFillColor: f => this.handleGetFillColor(f),
         getElevation: f => this.handleGetElevation(f),
       },
+      transitions: {
+        getFillColor: {
+          duration: 10,
+        },
+        getElevation: {
+          duration: 500,
+        },
+      },
     });
-  };
 
   render() {
     // State
     const {
-      index,
+      type, party,
     } = this.state;
 
     // Props
@@ -148,16 +164,21 @@ class Map extends Component {
             <DeckGL
               {...map.viewport}
               layers={this.renderLayers()}
-            />
-            <IndividualMarker {...individualMapLoop[index]} />
+              getCursor={this.handleGetCursor}
+            >
+              {this.renderTooltip()}
+            </DeckGL>
           </ReactMapGL>
         </Col>
-        <Col
-          xs={12}
-          mg={6}
-          lg={4}
-        >
-          <IndividualCard instance={individualMapLoop[index]} />
+        <Col>
+          <Input name="type" type="select" onChange={this.handleOnChange} value={type}>
+            <option value="GID_1">Electoral Area</option>
+            <option value="NAME_1">Province</option>
+          </Input>
+          <Input name="party" type="select" onChange={this.handleOnChange} value={party}>
+            <option value="totalValidVotes">Total</option>
+            <option value="partidoPRD">PRD</option>
+          </Input>
         </Col>
       </Row>
     );
