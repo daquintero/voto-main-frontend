4bbd3f7625ceb57d4ef4756058fe44ea584cd6e6
Dario A. Quintero D <darioaquintero@gmail.com>
2019-03-09 14:38:12 +0000

    Minor stylistic changes required

--- src/Assembly/Home/components/pdfPreviewer.jsx
+++ src/Assembly/Home/components/pdfPreviewer.jsx
@@ -1,51 +1,22 @@
-// Pdf Renderer
-import React, { PureComponent } from 'react';
-import axios from 'axios';
+import React from 'react';
 import PDFViewer from 'mgr-pdf-viewer-react';
 
-const manual = 'https://s3.amazonaws.com/votoinformado2019/Manual_Voto_Informado_2019.pdf';
+const manual = `${process.env.PUBLIC_URL}/Manual_Voto_Informado_2019.pdf`;
 
-// Declaration
-class Whatever extends PureComponent {
-  componentDidMount() {
-    this.getPDF();
-  }
+const PDF = () => (
+  <PDFViewer
+    document={{ url: manual }}
+    scale={0.9}
+    css="customViewer"
+    navigation={{
+      css: {
+        previousPageBtn: 'customPrevBtn',
+        nextPageBtn: 'customNextBtn',
+        pages: 'customPages',
+        wrapper: 'customWrapper',
+      },
+    }}
+  />
+);
 
-  getPDF = () => {
-    axios.get('https://s3.amazonaws.com/votoinformado2019/Manual_Voto_Informado_2019.pdf')
-      .then((response) => {
-        // handle success
-        console.log(response);
-      })
-      .catch((error) => {
-        // handle error
-        console.log(error);
-      })
-      .then(() => {
-        // always executed
-      });
-  };
-
-  render() {
-    return (
-      <div>
-        <PDFViewer
-          document={{ url: manual }}
-          scale={0.9}
-          css="customViewer"
-          navigation={{
-            css: {
-              previousPageBtn: 'customPrevBtn',
-              nextPageBtn: 'customNextBtn',
-              pages: 'customPages',
-              wrapper: 'customWrapper',
-            },
-          }}
-        />
-      </div>
-    );
-  }
-}
-
-// TODO State Store Connection
-export default Whatever;
+export default PDF;
