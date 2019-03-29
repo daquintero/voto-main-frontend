9fe81da8a90b92ac4bfb9a1b9975aef4a2df37b9
Dario Quintero <darioaquintero@gmail.com>
2019-03-29 21:01:42 +0000

    Card parent info improvements

--- src/Reusable/Individual/components/Controversy/components/Card.jsx
+++ src/Reusable/Individual/components/Controversy/components/Card.jsx
@@ -19,15 +19,15 @@ class ContCard extends PureComponent {
       <div className={`${light ? 'bg-layout' : 'bg-shady-layout'} shadow small-enlarge`}>
         <a target="_blank" rel="noreferrer noopener" href={instance.source}>
           <Card className="border-0 rounded-0">
+            <CardBody className="p-2">
+              <p className="p-1 m-0">{instance.briefDescription}</p>
+            </CardBody>
             {categoryInfo[instance.type] ? (
               <CardHeader className="pt-1 pb-1 ">
                 <i className={`float-left fa p-1 fa-${categoryInfo[instance.type].icon}`} />
                 <p className="small float-right mb-0">{categoryInfo[instance.type].Spanish}</p>
               </CardHeader>
-          ) : null}
-            <CardBody className="p-2">
-              <p className="p-1 m-0">{instance.briefDescription}</p>
-            </CardBody>
+            ) : null}
           </Card>
         </a>
       </div>
