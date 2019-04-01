b4c28d592784a2ed57e902be5f998462ea430948
JamieWilliamsHackIT <mail@jamiewilliams.co.uk>
2019-04-01 02:36:02 +0100

    Remove error boundary

--- src/Assembly/DetailedPage/components/Individual/tests/Individual.test.jsx
+++ src/Assembly/DetailedPage/components/Individual/tests/Individual.test.jsx
@@ -1,19 +1,22 @@
 // Test for IndividualPage Page Component
 import React from 'react';
 import renderer from 'react-test-renderer';
+import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import IndividualPage from '../Page';
 import store from '../../../../../App/store';
 
 describe('<IndividualPage />', () => {
   const initialState = store;
-  const match = { params: { id: 1  }, path: '/'  };
+  const match = { params: { id: 1 }, path: '/' };
 
   it(' renders correctly without crashing', () => {
     const tree = renderer
       .create(<>
         <Provider store={initialState}>
-          <IndividualPage match={match} />
+          <BrowserRouter>
+            <IndividualPage match={match} />
+          </BrowserRouter>
         </Provider>
       </>)
       .toJSON();
