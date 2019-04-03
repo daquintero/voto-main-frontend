23e17ca1f2e96f2c97ae0e41ad3c2dbd1c29036d
Dario A. Quintero D <daquintero@Darios-MacBook-Pro-2.local>
2019-04-03 21:19:35 +0100

    Improved about page

--- src/Assembly/AboutUsPage/index.jsx
+++ src/Assembly/AboutUsPage/index.jsx
@@ -8,20 +8,26 @@ import Social from './Social';
 import Who from './Who';
 import Thanks from './Thanks';
 import Members from './Members';
+import Video from '../Video';
 
 // Declaration
 const AboutPage = () => (
-  <Container fluid className="text-center p-5 bg-shady-layout">
-    <Row>
-      <Col xs={12}>
-        <h2>#VotoInformado2019</h2>
-      </Col>
-    </Row>
-    <Who />
-    <Thanks />
-    <Members />
-    <Social />
-  </Container>
+    <>
+      <Container fluid className="text-center p-3 py-5 p-md-5 bg-shady-layout">
+        <Row>
+          <Col xs={12}>
+            <h2>#VotoInformado2019</h2>
+          </Col>
+        </Row>
+      </Container>
+      <Container className="py-5">
+        <Video />
+        <Who />
+        <Thanks />
+        <Members />
+        <Social />
+      </Container>
+    </>
 );
 
 // TODO State Store Connection
