9b8072f1db02a752473209f26e61182fb1ced5fb
Dario Quintero <darioaquintero@gmail.com>
2019-04-18 19:13:05 +0100

    Improvements to loading speed

--- src/Assembly/Home/index.jsx
+++ src/Assembly/Home/index.jsx
@@ -7,7 +7,6 @@ import { Container, Row, Col } from 'reactstrap';
 // Components
 import Video from '../Video';
 import Commit from './components/Commit';
-import PDF from './components/pdfPreviewer';
 import Candidates from './components/Presidential';
 import Search from './components/Search';
 import ErrorBoundary from '../../shared/components/ErrorBoundary';
@@ -17,7 +16,7 @@ import { getHome } from './redux/actions';
 import Discovery from './components/Discovery';
 
 
-// const sep = `${process.env.PUBLIC_URL}/img/star-separator.svg`;
+const manual = `${process.env.PUBLIC_URL}/img/manual.JPG`;
 
 
 class Home extends PureComponent {
@@ -110,7 +109,14 @@ class Home extends PureComponent {
                     ¡Descarga el Manual de Cívica Electoral Gratis!
                   </a>
                 </h4>
-                <PDF />
+                <a
+                  target="_blank"
+                  rel="noreferrer noopener"
+                  href="https://s3.amazonaws.com/votoinformado2019/Manual_Voto_Informado_2019.pdf"
+                  className="p-2"
+                >
+                  <img src={manual} alt="" className="w-100 w-md-75 p-2 justify-content-center" />
+                </a>
               </Col>
             </Row>
           </Container>
